7d4d80dea61a3292ae9fd2a119a9436cf4c4e2fa
stetaf <s.tafuri95@gmail.com>
2021-07-14 17:28:18 +0200

    Add required fields attribute

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,11 +6,11 @@
             @csrf
             <div class="form-group">
                 <label for="name">Name</label>
-                <input type="text" name="name" class="form-control" id="name" placeholder="Enter the name">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Enter the name" required>
             </div>
             <div class="form-group">
                 <label for="serie">Series</label>
-                <input type="text" name="serie" class="form-control" id="serie" placeholder="Enter the series">
+                <input type="text" name="serie" class="form-control" id="serie" placeholder="Enter the series" required>
             </div>
             <div class="form-group">
                 <label for="category">Category</label>
@@ -18,7 +18,7 @@
             </div>
             <div class="form-group">
                 <label for="price">Price</label>
-                <input type="number" name="price" class="form-control" id="price" placeholder="Enter the price" step="0.01">
+                <input type="number" name="price" class="form-control" id="price" placeholder="Enter the price" step="0.01" required>
             </div>
             <div class="form-group">
                 <label for="release_date">Release date</label>
@@ -26,11 +26,11 @@
             </div>
             <div class="form-group">
                 <label for="isbn">ISBN</label>
-                <input type="number" name="isbn" class="form-control" id="isbn" placeholder="Enter the isbn">
+                <input type="number" name="isbn" class="form-control" id="isbn" placeholder="Enter the isbn" required>
             </div>
             <div class="form-group">
                 <label for="poster">Poster url</label>
-                <input type="text" name="poster" class="form-control" id="poster" placeholder="Enter the poster url">
+                <input type="text" name="poster" class="form-control" id="poster" placeholder="Enter the poster url" required>
             </div>
             <button type="submit" class="btn btn-primary">Add comic</button>
         </form>
