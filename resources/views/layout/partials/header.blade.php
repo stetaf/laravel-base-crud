5740320e71a5c6dba7ddaff7d1efc8d94160ed89
stetaf <s.tafuri95@gmail.com>
2021-07-14 16:05:28 +0200

    Add navbar links with their own route

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -1,5 +1,20 @@
 <div class="container">
     <nav>
-        <h1>Boolean Comics</h1>
+        <h1>
+            <a href="{{ route('home') }}">Boolean Comics</a>
+        </h1>
+        <div class="links">
+            <ul>
+                <li>
+                    <a href="{{ route('home') }}">Home</a>
+                </li>
+                <li>
+                    <a href="{{ route('comics.index') }}">Comics</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 </div>
