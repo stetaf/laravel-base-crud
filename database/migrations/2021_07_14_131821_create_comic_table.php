526d17917dca75442856e35fd667f1859e9f8f6c
stetaf <s.tafuri95@gmail.com>
2021-07-14 15:25:02 +0200

    Create comics migration

---
+++ database/migrations/2021_07_14_131821_create_comic_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('serie');
+            $table->string('category')->nullable();
+            $table->float('price', 3, 2);
+            $table->string('release_date')->nullable();
+            $table->bigInteger('isbn');            
+            $table->string('poster');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comic');
+    }
+}
