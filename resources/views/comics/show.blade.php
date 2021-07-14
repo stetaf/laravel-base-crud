11b09c0524cb1325fd2ab0b7ee9c73fa81ab179f
stetaf <s.tafuri95@gmail.com>
2021-07-14 17:02:00 +0200

    Show single comic info

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="container">
+        <div class="comic">
+            <div class="card">
+                <img src="{{ $comic->poster }}" alt="">
+            </div>
+            <div class="info">
+                <h3>{{$comic->name }}</h3>
+                <span>
+                    <b>Serie: </b>{{ $comic->serie}}
+                </span>
+                <span>
+                    <b>Category: </b>{{ $comic->category}}
+                </span>
+                <span>
+                    <b>Release date: </b>{{ $comic->realease_date}}
+                </span>
+                <span>
+                    <b>ISBN: </b>{{ $comic->isbn}}
+                </span>
+                <div class="buy">
+                    <span>
+                        <b>Price: $</b>{{ $comic->price}}
+                    </span>
+                    <span>
+                        <a href="#" class="btn btn-secondary">Buy now</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
