1e328e40db59698c04474094e50a629423501cd0
stetaf <s.tafuri95@gmail.com>
2021-07-14 16:05:05 +0200

    Name home route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PageController@index');
+Route::get('/', 'PageController@index')->name('home');
 
 Route::resource('comics', 'ComicController');
