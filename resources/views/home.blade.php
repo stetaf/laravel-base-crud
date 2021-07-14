5bb09919397d00a4c8912ab19970b8fda4e4239f
stetaf <s.tafuri95@gmail.com>
2021-07-14 16:11:09 +0200

    Add basic text to the homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,18 @@
 @extends('layout.app')
 
 @section('content')
-    <h1>Home</h1>
+    <div class="container">
+        <h1>Home</h1>
+
+        <h3>
+            We currently have 
+            @if ($total > 1)
+                <span class="num">{{ $total }}</span> comics listed!
+            @else
+                <span class="num">{{ $total }}</span> comic listed!
+            @endif
+            <br>
+            Browse the list through the navbar!
+        </h3>
+    </div>
 @endsection
